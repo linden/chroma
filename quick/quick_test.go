d993fd2a34c02ec14a33af3e1e123b1497223882
Linden <70739041+linden@users.noreply.github.com>
2022-02-10 03:20:29 -0800

    feat: add `Clarity` support

--- quick/quick_test.go
+++ quick/quick_test.go
@@ -18,3 +18,33 @@ func main() { }
 		log.Fatal(err)
 	}
 }
+
+func TestClarityHighlight(t *testing.T) {
+	code := `(define-read-only (example uint)
+	(begin
+		;; print something
+		(print example)
+		
+		;; example
+		(asserts! (is-eq 1 1) (err 1))
+		
+		;; return something
+		(ok some("example"))
+	)
+)
+
+(example 100)
+`
+
+	file, err := os.OpenFile("output.html", os.O_RDWR|os.O_CREATE, 0755)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = quick.Highlight(file, code, "clarity", "html", "monokai")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
